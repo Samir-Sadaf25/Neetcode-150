d843e9772c155cde35c7b03982b9f838b2bbe7f0
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-09 14:19:57 +0600

    optimal solution(O(N): Valid Anagram using using frquency array

--- Arrays&Hashing/Valid Anagram.cpp
+++ Arrays&Hashing/Valid Anagram.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         int n = s.length();
+//         if(s.length() != t.length())
+//            return false;
+//        else if(s == t)
+//             return true;
+//         else return false;    
+          
+//     }
+// };
+
+// Optimize solution :
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int n = s.length();
-        if(s.length() != t.length())
-           return false;
-       else if(s == t)
-            return true;
-        else return false;    
+        int count[26] = {0};
+        for(char c : s)
+               count[c-'a']++;
+        for(char c : t)
+                count[c-'a']--;
+        for(int x : count)
+        {
+            if(x != 0)
+              return false;
+        }
+        return true;         
           
     }
 };
