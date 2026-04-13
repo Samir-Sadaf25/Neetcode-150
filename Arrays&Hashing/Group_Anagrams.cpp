e8ea31c275ea7532f033763fd8deba20dd414301
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-13 13:55:47 +0600

    Group Anagrams: Optimal solution without sorting O(N*K)

--- Arrays&Hashing/Group_Anagrams.cpp
+++ Arrays&Hashing/Group_Anagrams.cpp
@@ -1,15 +1,53 @@
 // https://leetcode.com/problems/group-anagrams/description/
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         int n = strs.size();
+//         vector<vector<string>> result;
+//         unordered_map<string,vector<string>> mp;
+//         for(int i = 0; i < n; i++)
+//         {
+//             string temp = strs[i];
+//             sort(begin(temp),end(temp));
+//             mp[temp].push_back(strs[i]);
+//         }
+//         for(auto it : mp)
+//         {
+//             result.push_back(it.second);
+//         }
+//         return result;
+//     }
+//     //TC : O M*(nLogn)
+// };
+
+// Optimal solution 
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<vector<string>> result;
-        unordered_map<string,vector<string>> mp;
-        for(int i = 0; i < n; i++)
+    
+    string Genarate(string word)
+    { 
+        int arr[26] = {0};
+        for(char ch : word)
         {
-            string temp = strs[i];
-            sort(begin(temp),end(temp));
-            mp[temp].push_back(strs[i]);
+            arr[ch - 'a']++;
+        }
+        string new_word = "";
+        for(int i = 0; i < 26;i++)
+        {
+            int freq = arr[i];
+            new_word += string(freq,i+'a');
+        }  
+        return new_word;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        vector<vector<string>> result;
+        int n = strs.size();
+        for(int i = 0; i < n;i++)
+        {
+            string word = strs[i];
+            string new_word = Genarate(word);
+            mp[new_word].push_back(strs[i]);
         }
         for(auto it : mp)
         {
@@ -17,5 +55,5 @@ public:
         }
         return result;
     }
-    //TC : O M*(nLogn)
+    // TC : O(N * (K + 26)) where n is the length of the string..each string i traverse kth times..
 };
