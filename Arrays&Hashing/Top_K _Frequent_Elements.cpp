93974eac19ee32751eec3fb2d6432b7279558591
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-16 15:32:28 +0600

    Top K Frequent Elements: Optimal solution O(N) by using bucket sort

--- Arrays&Hashing/Top_K _Frequent_Elements.cpp
+++ Arrays&Hashing/Top_K _Frequent_Elements.cpp
@@ -1,20 +1,37 @@
 class Solution {
-public:
+public: // bucket sort
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int,int> mp;
-        vector<pair<int,int>> arr;
+        vector<vector<int>> bucket(n+1);
         for(auto num : nums)
             mp[num]++;
-        for(auto p : mp)
-           arr.push_back({p.second,p.first});
-        sort(arr.rbegin(),arr.rend());
-        vector<int> res;
-        for(int i = 0;i < k;i++)
+        for(auto it : mp)
         {
-            res.push_back(arr[i].second);
-        }       
-        return res;
-        // TC: O(NLogN)
-        // SC: O(N)
-    }
+            int freq = it.second;
+            int element = it.first;
+            bucket[freq].push_back(element); // bucket er index = map er frequency
+        }
+        vector<int> result;
+        // pick element from right -> left to find max frequencies elements
+          for(int i = n; i>= 0;i--)
+        {
+            if( k == 0) 
+                break;
+            if(!bucket[i].empty()) // bucket[i] empty na hoile element pick korbo
+            {
+                for(auto element : bucket[i])
+                {
+                    result.push_back(element);
+                    k--;
+                    if(k == 0) // jodi loop er vitore result paia jai return kore dibo 
+                      return result;
+                }
+            }    
+
+        }
+        return result;    
+    } 
+    //TC : O(N)
+    //SC : O(N)
 };
