20eed2217f5e59d19c714a15f54e5c63837492b7
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-04-23 15:42:20 +0600

    Product of Array Except Self: Naive approach using division O(N)

--- Arrays&Hashing/Products_of_Array_Except_Self.cpp
+++ Arrays&Hashing/Products_of_Array_Except_Self.cpp
@@ -1,19 +1,57 @@
-// Brute force solution
+      // Brute force solution
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> result(n,1);
+//         for(int i = 0;i < n;i++)
+//         {
+//             for(int j = 0;j<n;j++)
+//             {
+//                 if(j == i)
+//                   continue;
+//                 result[i] *=  nums[j];
+//             }
+//         }
+//         return result;
+//     }
+//     // TC : O(n^2)
+// };
+          //Naive Approach using Division 
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n,1);
-        for(int i = 0;i < n;i++)
+        int count_zero = 0;
+        int product_without_zero = 1;
+        for(auto num : nums)
         {
-            for(int j = 0;j<n;j++)
+            if(num == 0)
+               count_zero++;
+            else
+               product_without_zero *= num;   
+        }
+        vector<int> result(n);
+        for(int i = 0;i<n;i++)
+        {
+            int num = nums[i];
+            if(num != 0)
             {
-                if(j == i)
-                  continue;
-                result[i] *=  nums[j];
+                if(count_zero > 0)
+                   result[i] = 0;
+                else   
+                   result[i] = product_without_zero/num;
+            }
+            else
+            {
+                if(count_zero > 1)
+                   result[i] = 0;
+                else
+                   result[i] = product_without_zero;   
             }
         }
         return result;
     }
-    // TC : O(n^2)
+    // TC : O(N)
+    // SC : O(1)
 };
